772baba26582c7b1533fd8b2a6115299c071d06a
fg69827-cmyk <fg69827@gmail.com>
2025-11-03 21:10:59 +0200

    Ενημέρωση pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+basic.forever(function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    music.ringTone(262)
+    music.rest(music.beat(BeatFraction.Whole))
+    radio.sendValue("name", 0)
+    for (let δείκτη = 0; δείκτη <= 4; δείκτη++) {
+        radio.sendString("")
+    }
+})
