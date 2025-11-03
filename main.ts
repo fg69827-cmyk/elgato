aba9e436905a296dc28d75ee564f6a97a71448ad
fg69827-cmyk <fg69827@gmail.com>
2025-11-03 21:17:43 +0200

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+let λίστα: number[] = []
 basic.forever(function () {
-    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("A - E F D G E F ", 120), music.PlaybackMode.UntilDone)
     music.ringTone(262)
+    for (let index = 0; index < 4; index++) {
+    	
+    }
     music.rest(music.beat(BeatFraction.Whole))
     radio.sendValue("name", 0)
     for (let δείκτη = 0; δείκτη <= 4; δείκτη++) {
         radio.sendString("")
+        for (let τιμή of λίστα) {
+            led.plot(0, 0)
+        }
     }
 })
